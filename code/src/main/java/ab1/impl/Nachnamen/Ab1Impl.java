2bee875a4c38c7907e2888e4fdb14c19fd5fe93a
yahorpaulson <y.paulson@yahoo.com>
2024-05-02 10:46:15 +0200

    matrices multiplication implemented

--- code/src/main/java/ab1/impl/Nachnamen/Ab1Impl.java
+++ code/src/main/java/ab1/impl/Nachnamen/Ab1Impl.java
@@ -59,7 +59,19 @@ public class Ab1Impl implements Ab1 {
 
 	@Override
 	public int[][] mult(int[][] m1, int[][] m2) {
-		return new int[0][];
+		if (m1[0].length != m2.length) {
+			throw new IllegalArgumentException("Cannot be multiplied!");
+		}
+		int[][] result = new int[m1.length][m2[0].length];
+
+		for (int i = 0; i < m1.length; i++) {
+			for (int j = 0; j < m2[0].length; j++) {
+				for (int k = 0; k < m1[0].length; k++) {
+					result[i][j] += m1[i][k] * m2[k][j];
+				}
+			}
+		}
+		return result;
 	}
 	private int partition(IndexedValue[] data, int low, int high) {
 		IndexedValue pivot = data[high];
